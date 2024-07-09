b05d2fb8f41eb0b50b7f2cf3e9cf0665f638e6d3
Oluwakemi Richard <kemierichard@gmail.com>
2024-07-09 01:57:21 +0100

    fixing errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
+# class HomeController < ApplicationController
+#   def index
+#   end
+# end
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
+    render json: { message: 'Testing!' }
   end
 end
