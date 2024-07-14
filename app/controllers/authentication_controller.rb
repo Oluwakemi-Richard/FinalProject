d4bd90907886086b17434d031c0620fb3fd2b280
Oluwakemi Richard <kemierichard@gmail.com>
2024-07-14 21:47:49 +0100

    working on authorization

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,21 +1,21 @@
-# # class AuthenticationController < ApplicationController
-# #     before_action :authenticate_user!, except: [:login]
+class AuthenticationController < ApplicationController
+    before_action :authenticate_user!, except: [:login]
   
-# #     def login
-# #       user = User.find_for_database_authentication(email: params[:email])
-# #       if user&.valid_password?(params[:password])
-# #         token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
-# #         render json: { token: token }, status: :ok
-# #       else
-# #         render json: { error: 'Invalid Email or Password' }, status: :unauthorized
-# #       end
-# #     end
+    def login
+      user = User.find_for_database_authentication(email: params[:email])
+      if user&.valid_password?(params[:password])
+        token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
+        render json: { token: token }, status: :ok
+      else
+        render json: { error: 'Invalid Email or Password' }, status: :unauthorized
+      end
+    end
   
-# #     def logout
-# #       current_user.update(jti: SecureRandom.uuid)
-# #       render json: { message: 'Logged out successfully' }, status: :ok
-# #     end
-# #   end
+    def logout
+      current_user.update(jti: SecureRandom.uuid)
+      render json: { message: 'Logged out successfully' }, status: :ok
+    end
+  end
   
 # class AuthenticationController < ApplicationController
 #   skip_before_action :authenticate_request, only: [:login]
