45444d86ea8f5eb25b5faf1ffc38e89eaa93dbfb
Oluwakemi Richard <kemierichard@gmail.com>
2024-07-15 10:06:36 +0100

    working on authorization

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -1,19 +1,4 @@
 class JwtService
-    SECRET_KEY = ENV['DEVISE_JWT_SECRET_KEY']
-  
-    def self.encode(payload)
-      JWT.encode(payload, SECRET_KEY, 'HS256')
-    end
-  
-    def self.decode(token)
-      Rails.logger.debug "Decoding token: #{token}"
-      decoded_token = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
-      Rails.logger.debug "Decoded token: #{decoded_token}"
-      HashWithIndifferentAccess.new(decoded_token[0])
-    rescue JWT::DecodeError => e
-      Rails.logger.error "JWT Decode Error: #{e.message}"
-      nil
-    end
   end
   
   
